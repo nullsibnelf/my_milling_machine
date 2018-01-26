0900f81fa1555b08cb0b5a3113b674e7cf11cac0
Steffen Küllmer <30567862+nullsibnelf@users.noreply.github.com>
2018-01-26 10:57:51 +0100

    Add files via upload

---
+++ software/arduino/ESC_test.ino
@@ -0,0 +1,45 @@
+
+#include <Servo.h>
+
+#define Taster           9
+#define StatusLed       13
+
+Servo esc;
+
+
+void setup() 
+{
+  pinMode(StatusLed,   OUTPUT);
+  pinMode(Taster,      INPUT_PULLUP);
+  
+  Serial.begin(9600);                         // setup serial for debugging
+
+  esc.attach(11);
+  esc.writeMicroseconds(1000);
+}
+
+
+
+
+void loop()
+{
+
+
+
+  if (digitalRead(Taster) == 0)
+  {
+    Serial.println("T1");  
+    
+  int val;
+  val = 400;
+  val = map(val, 0, 1023, 1000, 2000);
+  esc.writeMicroseconds(val);
+
+    
+    while (digitalRead(Taster) == 0);
+  }
+
+}
+
+
+
